f0d47891facd0de84f0c0b1cf16ad4d1d3677203
hezudaopp <hezudaopp@163.com>
2017-08-01 17:45:03 +0800

    rsa jwt

--- src/main/java/com/youmayon/tutorial/config/ResourceServerConfig.java
+++ src/main/java/com/youmayon/tutorial/config/ResourceServerConfig.java
@@ -1,8 +1,6 @@
 package com.youmayon.tutorial.config;
 
-import com.youmayon.tutorial.constant.SecurityConstants;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
